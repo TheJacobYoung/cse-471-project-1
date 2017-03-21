f72bf34b8674e63a534925bb68977e8713945e90
daykinev <daykinev@msu.edu>
2017-03-21 13:33:11 -0400

    effects use pointers now

--- Synthie/Chorus.h
+++ Synthie/Chorus.h
@@ -6,10 +6,9 @@ public:
 	double* process() {
 		double wet[2] = { 0 };
 		double out[2] = { 0 };
-		double* dry = m_box.read_raw_frame();
-		int loc = m_box.getWrloc();
-		int delayIdx = loc - (1102 + (220* sin(2 * PI*0.25*loc))); //20-30ms
-		double* delayed = m_box.read_raw_frame(delayIdx);
+		double* dry = dry_q->read(0);
+		int delayIdx = *m_wrloc - (1102 + (220* sin(2 * PI*0.25*(*m_wrloc)))); //20-30ms
+		double* delayed = dry_q->read(delayIdx);
 		wet[0] = 0.5*dry[0] + 0.5*delayed[0];
 		wet[1] = 0.5*dry[1] + 0.5*delayed[1];
 		out[0] = m_wet*wet[0] + m_dry*dry[0];
--- Synthie/Gate.h
+++ Synthie/Gate.h
@@ -9,7 +9,7 @@ public:
 	double* process() {
 		double wet[2] = { 0 };
 		double out[2] = { 0 };
-		double* dry = dry_q.read(0);
+		double* dry = dry_q->read(0);
 		if (dry[0] < m_threshold) {//attenuate samples below threshold
 			wet[0] = 0.1*dry[0];
 			wet[1] = 0.1*dry[1];
